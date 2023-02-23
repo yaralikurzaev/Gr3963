b182366a895215ac90c38e4314b488cfba6e130a
Yarali Kurzaev <legimusic@mail.ru>
2023-02-23 17:23:06 +0300

    Сделал домашнее задание: Задача 23

---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// * Вывести таблицу с границами и значениями друг над другом
+
+//принимаем число от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//метод возводящий в степень и возвращает строку
+string LineBuilder(int num, int pow)
+{
+    string res = String.Empty;
+    for (int i = 1; i <= num; i++)
+    {
+        res = res + Math.Pow(i, pow) + " \t |";
+    }
+    return res;
+}
+//решение задачи
+int nOf = ReadData("Введите число до какого числа показать: ");
+int pOf = ReadData("Введите в какую степень возввести числа (для решения задания введите `3`): ");
+Console.WriteLine(LineBuilder(nOf, 1));
+Console.WriteLine(LineBuilder(nOf, pOf));
