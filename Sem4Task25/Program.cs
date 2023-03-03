48f73e85a75f112a4960c499dbebbb7b05090d53
Yarali Kurzaev <legimusic@mail.ru>
2023-03-03 15:10:05 +0300

    Сделал домашнее задание: Задача 25

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// №25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в
+// натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// * Написать калькулятор с операциями +, -, /, * и возведение в степень
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+double Pow(int a, int b)
+{
+    double res = Math.Pow(a,b);
+    return res;
+}
+
+double stepen = Pow(ReadData("Введите число: "),ReadData("Введите степень в которую хотите возвести: "));
+Console.WriteLine("Результат: "+ stepen);
