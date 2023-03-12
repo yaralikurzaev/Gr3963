4c4dd6840af71b07f29eef83204d8d6133e437b8
Yarali Kurzaev <legimusic@mail.ru>
2023-03-12 16:41:10 +0300

    Сделал домашнее задание: Задача 56

---
+++ Sem8Task56/Program.cs
@@ -0,0 +1,91 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+//Принимаем из консоли число
+int ReadInputInt(string mess)
+{
+    Console.Write(mess);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+
+//Метод, который генерирует массив случайными числами
+int[,] Gen2DArrRandom(int row, int colum, int bottom, int top)
+{
+    Random rnd = new Random();
+    int[,] arr = new int[row, colum];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < colum; j++)
+        {
+            arr[i, j] = rnd.Next(bottom, top);
+        }
+    }
+    return arr;
+}
+
+// выводим двумерный массив на экран 
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+string FindSumRow2DArr(int[,] arr)
+{
+    int[] buf = new int[arr.GetLength(0)];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            buf[i] += arr[i, j];
+        }
+    }
+
+    int minIndex = FindMinimumElement(buf);
+
+    return $"Строка с наименьшей суммой элементов - {minIndex+1} ";
+}
+
+int FindMinimumElement(int[] arr)
+{
+    int min = arr[0];
+    int minIndex = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+}
+
+
+int countRow = ReadInputInt("Введите количество строк для двумерного массива: ");
+int countColum = ReadInputInt("Введите количество столбцов для двумерного массива: ");
+int min = ReadInputInt("Введите 1 границу последовательности: ");
+int max = ReadInputInt("Введите 2 границу последовательности: ");
+if (min > max) (min, max) = (max, min);
+
+int[,] mas = Gen2DArrRandom(countRow, countColum, min, max);
+
+Console.WriteLine("Исходный массив:");
+Print2DArr(mas);
+Console.WriteLine(FindSumRow2DArr(mas));
